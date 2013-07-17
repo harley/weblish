80ddddca7b9b8ae5e70d106f56313a41ce29ca52
Harley Trung <harley@socialsci.com>
2013-07-17 14:31:40 +0700

    update documents#index. add fog

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,11 +1,17 @@
 module DocumentsHelper
-  def render_document(document)
+  def render_document(document, options = {})
     case document.file.file.extension
     when "png", "jpg", "gif"
       image_tag(document.file)
     when "pdf"
-      content_tag(:object, data: document.file.url, type: 'application/pdf', width: '100%', height: '100%', style:'min-height:826px') do
-        "This browser doesn't support displaying PDF"
+      if options[:preview]
+        content_tag(:object, data: document.file.url, type: 'application/pdf', width: '100%') do
+          "This browser doesn't support displaying PDF"
+        end
+      else
+        content_tag(:object, data: document.file.url, type: 'application/pdf', width: '100%', height: '100%', style:'min-height:826px') do
+          "This browser doesn't support displaying PDF"
+        end
       end
     else
       "Preview Not Available"
