4e9e80e61b7f7e7879bf23aff3b7beeac47523dc
Harley Trung <harley@socialsci.com>
2013-07-18 14:38:40 +0700

    add title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def javascripts(&block)
     content_for :javascripts, &block
   end
+
+  def title(page_title, show_title = true, different_title_symbol = nil)
+    unless @show_title # prevent overriding when called multiple times
+      title_symbol = different_title_symbol || :title
+      content_for(title_symbol) { page_title.to_s } if page_title.present?
+      @show_title = show_title
+    end
+    nil # output nothing
+  end
+
+  def show_title?
+    @show_title
+  end
 end
