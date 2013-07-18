833b92c4c38c68f17e52b2307676e50d707565b3
Harley Trung <harley@socialsci.com>
2013-07-18 11:49:27 +0700

    auto set document name from filename

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,14 +3,21 @@ class Document < ActiveRecord::Base
   mount_uploader :file, DocumentUploader
 
   validates :file, presence: true
-  validates :name, presence: true
   validates :user_id, presence: true
 
   default_scope -> { order('created_at DESC') }
 
   acts_as_commentable
 
+  after_save :set_name_if_not_presence
+
   def to_s
     name || file
+  end
+
+  def set_name_if_not_presence
+    if self.name.blank? && file?
+      self.name = file.file.filename
+    end
   end
 end
